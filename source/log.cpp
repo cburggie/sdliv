6711a37a29ffb9ba7c07a950193e84e1e5fd8de1
Eric Gibison <eric.gibison@gmail.com>
2020-05-02 15:24:46 -0700

    Rewrote sdliv::log to take variable number of entries of variable type

--- source/log.cpp
+++ source/log.cpp
@@ -1,26 +1,9 @@
-#include <sdliv.h>
+#include <log.h>
 
-#include <string>
 #include <iostream>
 
 
-void sdliv::log(const char * s, const char * arg)
+void sdliv::log()
 {
-	std::cout << s;
-	if (arg != nullptr) std::cout << ": " << arg;
 	std::cout << std::endl;
-}
-
-void sdliv::log(const char * s, const std::string & arg)
-{
-	log(s,arg.c_str());
-}
-
-void sdliv::log(const std::string & s, const char * arg)
-{
-	log(s.c_str(), arg);
-}
-void sdliv::log(const std::string & s, const std::string & arg)
-{
-	log(s.c_str(),arg.c_str());
 }
